d1979c649a969157baec0f61d6d8910f8c967a82
= <fabombielar@correo.udistrital.edu.co>
2021-05-30 15:18:08 -0500

    Agregando definicion de criterios de aceptacion de las primeras 6 historias Agregando libreria Requests Agregando steps para los escenarios

--- pruebas/features/pruebas_Aceptacion.feature
+++ pruebas/features/pruebas_Aceptacion.feature
@@ -1,6 +1,63 @@
-Feature: Aplicacion
+Feature: App de estimacion
 
-  Scenario: Como usuario regular quiero crear un proyecto para manejar el proceso de estimación
-    Given esta la aplicacion arriba
-    When envio una peticion a la siguiente url "dummy_url"
+  Scenario Outline: Como usuario regular quiero crear un proyecto para manejar el proceso de estimación
+    Given la <url> para la creacion
+    When el usuario regular desee "crear" un proyecto con <nombre_proyecto> y <descripcion_proyecto>
     Then el sistema almacenara un nuevo proyecto
+
+    Examples: proyectos
+      | nombre_proyecto | descripcion_proyecto        |
+      | proyecto1       | Este es el super proyecto 1 |
+      | proyecto2       | Este es el super proyecto 2 |
+      | proyecto3       | Este es el super proyecto 3 |
+      | proyecto4       | Este es el super proyecto 4 |
+
+  Scenario Outline: Como usuario regular quiero consultar un proyecto por id para ver sus módulos y funciones
+    Given un <proyecto> existente
+    When envio una peticion a la siguiente "url"
+    Then el sistema retornara el proyecto con sus modulos y funciones
+
+    Examples: proyectos
+      | proyecto |
+      | 1        |
+      | 2        |
+      | 3        |
+      | 4        |
+
+  Scenario Outline: Como usuario regular quiero actualizar el nombre y la descripción de un proyecto para manejar el proceso de estimación
+    Given un <proyecto> existente
+    When el usuario regular desee "actualizar" un proyecto con <nombre_proyecto> y <descripcion_proyecto>
+    Then el sistema almacenara los cambios del proyecto
+
+    Examples: actualizacion
+      | proyecto | nombre_proyecto      | descripcion_proyecto                    |
+      | 1        | proyecto1Actualizado | Este es el super proyecto 1 actualizado |
+      | 2        | proyecto2Actualizado | Este es el super proyecto 2 actualizado |
+      | 3        | proyecto3Actualizado | Este es el super proyecto 3 actualizado |
+      | 4        | proyecto4Actualizado | Este es el super proyecto 4 actualizado |
+
+  Scenario: Como usuario regular quiero ver el listado de proyectos para consultar su nombre y descripcion.
+    Given esta la aplicacion arriba
+    When envio una peticion a la siguiente url "get_all"
+    Then el sistema retornara todos los proyectos con su nombre y descripcion
+
+  Scenario Outline: Como usuario regular quiero crear un módulo para asociarlo a un proyecto
+    Given un <proyecto> existente
+    When envio una peticion a la siguiente url "dummy_url" para crear modulos del <proyecto> con los datos <nombre_modulo> y <descripcion_modulo>
+    Then el sistema almacenara un nuevo modulo en el proyecto
+    Examples: actualizacion
+      | proyecto | nombre_modulo | descripcion_modulo               |
+      | 1        | modulo1Prueba | Este es el super modulo 1 creado |
+      | 2        | modulo2Prueba | Este es el super modulo 2 creado |
+      | 3        | modulo3Prueba | Este es el super modulo 3 creado |
+      | 4        | modulo4Prueba | Este es el super modulo 4 creado |
+
+  Scenario: Como usuario regular quiero borrar el módulo asociado a un proyecto por Id para eliminar todas las funciones asociadas
+    Given un <proyecto> existente
+    And contiene modulos
+    When envio una peticion a la siguiente url "dummy_url" <proyecto>
+    Then el sistema eliminara el modulo del proyecto
+
+
+
+
--- pruebas/steps/steps.py
+++ pruebas/steps/steps.py
@@ -1,17 +1,138 @@
 from behave import *
 
-@given('esta la aplicacion arriba')
-def step_impl(context):
-    pass
-
-@given('voy vender bonice')
-def step_impl(context):
-    pass
-
-@when('envio una peticion a la siguiente url {url}')
-def step_impl(context, url):
-    print(url)
 
 @then('el sistema almacenara un nuevo proyecto')
 def step_impl(context):
     assert context.failed is False
+
+
+@given("la {url} para la creacion")
+def step_impl(context, url):
+    """
+    :type context: behave.runner.Context
+    :type url: str
+    """
+    raise NotImplementedError(u'STEP: Given la <url> para la creacion')
+
+
+@when('el usuario regular desee "crear" un proyecto con {nombre_proyecto} y {descripcion_proyecto}')
+def step_impl(context, nombre_proyecto, descripcion_proyecto):
+    """
+    :type context: behave.runner.Context
+    :type nombre_proyecto: str
+    :type descripcion_proyecto: str
+    """
+    raise NotImplementedError(
+        u'STEP: When el usuario regular desee "crear" un proyecto con <nombre_proyecto> y <descripcion_proyecto>')
+
+
+@then("el sistema retornara el proyecto con sus modulos y funciones")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Then el sistema retornara el proyecto con sus modulos y funciones')
+
+
+@then("el sistema almacenara los cambios del proyecto")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Then el sistema almacenara los cambios del proyecto')
+
+
+@then("el sistema retornara todos los proyectos con su nombre y descripcion")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Then el sistema retornara todos los proyectos con su nombre y descripcion')
+
+
+@then("el sistema almacenara un nuevo modulo en el proyecto")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Then el sistema almacenara un nuevo modulo en el proyecto')
+
+
+@step("contiene modulos")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: And contiene modulos')
+
+
+@then("el sistema eliminara el modulo del proyecto")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Then el sistema eliminara el modulo del proyecto')
+
+
+@when('envio una peticion a la siguiente "url"')
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: When envio una peticion a la siguiente "url"')
+
+
+@given("un {proyecto} existente")
+def step_impl(context, proyecto):
+    """
+    :type context: behave.runner.Context
+    :type proyecto: str
+    """
+    raise NotImplementedError(u'STEP: Given un <proyecto> existente')
+
+
+@given("esta la aplicacion arriba")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Given esta la aplicacion arriba')
+
+
+@when('envio una peticion a la siguiente url "get_all"')
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: When envio una peticion a la siguiente url "get_all"')
+
+
+@when('el usuario regular desee "actualizar" un proyecto con {nombre_proyecto} y {descripcion_proyecto}')
+def step_impl(context, nombre_proyecto, descripcion_proyecto):
+    """
+    :type context: behave.runner.Context
+    :type nombre_proyecto: str
+    :type descripcion_proyecto: str
+    """
+    raise NotImplementedError(
+        u'STEP: When el usuario regular desee "actualizar" un proyecto con <nombre_proyecto> y <descripcion_proyecto>')
+
+
+@when('envio una peticion a la siguiente url "dummy_url" para crear modulos del {proyecto} con los datos {nombre_modulo} y {descripcion_modulo}')
+def step_impl(context, proyecto, nombre_modulo, descripcion_modulo):
+    """
+    :type context: behave.runner.Context
+    :type proyecto: str
+    :type nombre_modulo: str
+    :type descripcion_modulo: str
+    """
+    raise NotImplementedError(
+        u'STEP: When envio una peticion a la siguiente url "dummy_url" para crear modulos del <proyecto> con los datos <nombre_modulo> y <descripcion_modulo>')
+
+
+@when('envio una peticion a la siguiente url "dummy_url" <proyecto>')
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: When envio una peticion a la siguiente url "dummy_url" <proyecto>')
