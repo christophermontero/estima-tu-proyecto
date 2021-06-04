e37c8f734e3c15ec5a2e7d9dd8a0a574f1604e12
sabrinasuarezarrieta <sabrinasuarezarrieta@gmail.com>
2021-06-03 21:00:25 -0500

    [el equipo modulos desarrolla el crear modulo y obtener modulos de un proyecto por id]

--- modulos/app.py
+++ modulos/app.py
@@ -1,5 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 from db import db_session, init_db
-from model import TestTable
+from model import Modulo 
+
+
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
 init_db()
+
+
+@app.route("/modulos", methods=["POST"])
+def create_modulos():
+    data = request.json
+    #return jsonify({"modulos": "hola"})
+    if data["nombreModulo"] is None:
+        return jsonify({"mensaje": "error"}), 400
+
+    modulo = Modulo.create(
+        data["idModulo"], data["nombreModulo"], data["descModulo"], data["proyecto_id"]
+    )
+
+    return jsonify({"modulos": modulo.toJson()})
+    
+
+
+@app.route("/modulos/<proyectoId>", methods=["GET"])
+def get_user(proyectoId):
+
+    m = [modulo.toJson() for modulo in Modulo.query.filter_by(proyecto_id=proyectoId).all()]
+
+    return jsonify({"modulos": m})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- modulos/model.py
+++ modulos/model.py
@@ -1,13 +1,7 @@
-from flask_sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
-
-
-class Proyecto(Base):
-    __tablename__ = 'proyectos'
-    idProyecto = Column(Integer, primary_key=True, autoincrement=True)
-    nombreProyecto = Column(String(50))
-    descProyecto = Column(String(150))
-    modulos = relationship('modulos', backref='proyectos', lazy=True)
+from db import db_session, init_db
 
 
 class Modulo(Base):
@@ -16,7 +10,39 @@ class Modulo(Base):
     nombreModulo = Column(String(50))
     descModulo = Column(String(150))
     proyecto_id = Column(Integer, ForeignKey('proyectos.idProyecto'))
-    funciones = relationship('Funcion', backref='modulos', lazy=True)
+    #funciones = relationship('Funcion', backref='modulos', lazy=True)
+
+    @classmethod
+    def create(cls, idModulo, nombreModulo, descModulo, proyecto_id):
+        modulo = Modulo(idModulo=idModulo, nombreModulo=nombreModulo, descModulo=descModulo, proyecto_id=proyecto_id)
+        return modulo.save()
+
+    def save(self):
+        try:
+            db_session.add(self)
+            db_session.commit()
+
+            return self
+        except:
+            return False
+
+    def toJson(self):
+        return {
+            "idModulo": self.idModulo,
+            "nombreModulo": self.nombreModulo,
+            "descModulo": self.descModulo,
+            "proyecto_id": self.proyecto_id,
+        }
+
+
+
+class Proyecto(Base):
+    __tablename__ = 'proyectos'
+    idProyecto = Column(Integer, primary_key=True, autoincrement=True)
+    nombreProyecto = Column(String(50))
+    descProyecto = Column(String(150))
+    #modulos = relationship('modulos', backref='proyectos', lazy=True)
+
 
 class Funcion(Base):
     __tablename__ = 'funciones'
