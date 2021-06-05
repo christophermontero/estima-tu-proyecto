402d84a916dad95b2359dfc3c5ca6d1ade8a644c
christophermontero <chris.to20@hotmail.com>
2021-06-04 19:39:28 -0500

    Se insertan las relaciones en todos los modelos

--- funciones/model.py
+++ funciones/model.py
@@ -5,41 +5,20 @@ from db import db_session, init_db
 
 
 class Proyecto(Base):
-    __tablename__ = "proyectos"
+    __tablename__ = "proyecto"
     idProyecto = Column(Integer, primary_key=True, autoincrement=True)
     nombreProyecto = Column(String(50))
     descProyecto = Column(String(150))
-    # modulos = relationship("modulos", backref="proyectos", lazy=True)
-
-    @classmethod
-    def create(cls, idProyecto, nombreProyecto, descProyecto):
-        proyecto = Proyecto(idProyecto=idProyecto, nombreProyecto=nombreProyecto, descProyecto=descProyecto)
-        return proyecto.save()
-
-    def save(self):
-        try:
-            db_session.add(self)
-            db_session.commit()
-
-            return self
-        except:
-            return False
-
-    def toJson(self):
-        return {
-            "idProyecto": self.idProyecto,
-            "nombreProyecto": self.nombreProyecto,
-            "descProyecto": self.descProyecto,
-        }
+    modulos = relationship("Modulo", backref="proyecto", lazy=True)
 
 
 class Modulo(Base):
-    __tablename__ = "modulos"
+    __tablename__ = "modulo"
     idModulo = Column(Integer, primary_key=True, autoincrement=True)
     nombreModulo = Column(String(50))
     descModulo = Column(String(150))
-    proyecto_id = Column(Integer, ForeignKey("proyectos.idProyecto"))
-    # funciones = relationship("Funcion", backref="modulos", lazy=True)
+    proyecto_id = Column(Integer, ForeignKey("proyecto.idProyecto"))
+    funciones = relationship("Funcion", backref="modulo", lazy=True)
 
 
     def toJson(self):
@@ -49,13 +28,13 @@ class Modulo(Base):
 
 
 class Funcion(Base):
-    __tablename__ = "funciones"
+    __tablename__ = "funcion"
     idFuncion = Column(Integer, primary_key=True, autoincrement=True)
     nombreFuncion = Column(String(50))
     numCampos = Column(Integer)
     numObjetos = Column(Integer)
     complejidad = Column(String(8))
-    modulo_id = Column(Integer, ForeignKey("modulos.idModulo"))
+    modulo_id = Column(Integer, ForeignKey("modulo.idModulo"))
 
     @classmethod
     def create(cls, idFuncion, nombreFuncion, numCampos, numObjetos, complejidad, modulo_id):
