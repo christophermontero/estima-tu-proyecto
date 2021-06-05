410ba60712f557f900353e6e0c012f7a4d9d8016
christophermontero <chris.to20@hotmail.com>
2021-06-04 21:37:30 -0500

    Se incluye la lógica del rollback

--- funciones/model.py
+++ funciones/model.py
@@ -9,7 +9,7 @@ class Proyecto(Base):
     idProyecto = Column(Integer, primary_key=True, autoincrement=True)
     nombreProyecto = Column(String(50))
     descProyecto = Column(String(150))
-    # modulos = relationship("Modulo", backref="proyecto", lazy=True)
+    modulos = relationship("Modulo", back_populates="proyecto", lazy=True)
 
 
 class Modulo(Base):
@@ -17,8 +17,9 @@ class Modulo(Base):
     idModulo = Column(Integer, primary_key=True, autoincrement=True)
     nombreModulo = Column(String(50))
     descModulo = Column(String(150))
-    proyecto_id = Column(Integer)
-    # funciones = relationship("Funcion", backref="modulo", lazy=True)
+    proyecto_id = Column(Integer, ForeignKey("proyecto.idProyecto"))
+    funciones = relationship("Funcion", back_populates="_modulos", lazy=True)
+    proyecto = relationship("Proyecto", back_populates="modulos")
 
 class Funcion(Base):
     __tablename__ = "funcion"
@@ -27,7 +28,8 @@ class Funcion(Base):
     numCampos = Column(Integer)
     numObjetos = Column(Integer)
     complejidad = Column(String(8))
-    modulo_id = Column(Integer)
+    modulo_id = Column(Integer, ForeignKey("modulo.idModulo"))
+    _modulos = relationship("Modulo", back_populates="funciones")
 
     @classmethod
     def create(cls, idFuncion, nombreFuncion, numCampos, numObjetos, complejidad, modulo_id):
@@ -60,6 +62,7 @@ class Funcion(Base):
 
             return self
         except:
+            db_session.rollback()
             return False
 
     def toJson(self):
@@ -79,4 +82,5 @@ class Funcion(Base):
 
             return True
         except:
+            db_session.rollback()
             return False
